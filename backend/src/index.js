0e3b05086b6023a22c50009ab0f358aa20248523
Furqan Ahmad <133624627+furqanahmadganaie@users.noreply.github.com>
2026-06-24 16:50:05 +0530

    prepare for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,21 @@
-// top of server.js / index.js
+
 import dns from "node:dns/promises";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-// then import mongoose/connect...
+
 
 import express from 'express';
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-// import path from "path";
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js'
 import { connectDB } from './lib/db.js';
 import { app,server} from "./lib/socket.js";
 
 dotenv.config({ quiet: true });
-// const app=express();
-// reolace app with server 
+
 
 const PORT = process.env.PORT || 4000;
-// const __dirname = path.resolve();
 
 
 app.use(express.json({limit:"10mb"})); //middleware  to ectrach the daata 
@@ -27,18 +24,12 @@ app.use(cookieParser()); //allows to parese the cookie
 //CqCFjIbMLu0g9XdW
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:  process.env.CLIENT_URL,
     credentials: true,
   })
 );
-// app.use(express.json({limit:"10mb"})); //middleware  to ectrach the daata 
-// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-// app.use(cookieParser()); //allows to parese the cookie
-// //CqCFjIbMLu0g9XdW
 
 
-
-// const PORT =3000;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 
